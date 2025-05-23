64d0352ba24576c204af7a128c760919b439a389
Enalika <essynalik@gmail.com>
2025-05-23 20:45:14 +0300

    Add file 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,24 @@
+
+// // const arg = process.argv.slice(2);
+// const x = parseInt(arg)
+
+// if (isNaN(x)){ 
+// console.log("Missing number of occurrences");
+// } else
+// let Displayed ="";
+// for (let x = 0; x < arg[0]; x++){ 
+// Displayed += 'C is fun \n'
+// console.log(Displayed.trim()); 
+// }
+const arg = process.argv.slice(2);
+const x = (arg[0]-1)
+
+if (isNaN(parseInt(x))){ 
+console.log("Missing number of occurrences");
+} else {
+let Displayed ="";
+for (let x = 0; x < arg[0]; x++){ 
+Displayed += 'C is fun \n'
+}
+console.log(Displayed.trim()); 
+}

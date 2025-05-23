60cb567bae7e298549a398d81663f14f0acaf6c0
Enalika <essynalik@gmail.com>
2025-05-23 19:36:21 +0300

    Add file 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -3,4 +3,4 @@ const Multilanguages = ["C is fun", "Python is cool", "JavaScript is amazing"]
 let i = "";
 for (const line of Multilanguages)
 i += line + "\n"
-    console.log(i);
+    console.log(i.trim());

068cb3ef77658d24e7a763195a54a007fad7b771
Enalika <essynalik@gmail.com>
2025-05-24 09:27:27 +0300

    Add file 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -5,9 +5,6 @@ const x = (arg[0])
 if (isNaN(x)){ 
 console.log("Missing number of occurrences");
 } else {
-let Displayed ="";
-for (let i = 0; i < x; i++){ 
-Displayed += 'C is fun\n'
-}
-console.log(Displayed.trim()); 
+for (let i = 0; i < x; i++)
+console.log('C is fun'); 
 }

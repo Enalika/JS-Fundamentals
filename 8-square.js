4cf9a60ba2d0088717b6702c3fd496f6dfe0b4db
Enalika <essynalik@gmail.com>
2025-05-24 09:56:14 +0300

    Add file 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,9 +1,13 @@
 const argument = process.argv.slice(2);
-const X = parseInt(argument[0])
-if (isNaN(X)){
-    console.log("Missing size")
+const X = parseInt(argument[0]);
+
+if (isNaN(X)) {
+    console.log("Missing size");
+
 } else {
-for( let i = 0; i < X; i++){
-    console.log('X'.repeat(X))
+
+for (let i = 0; i < X; i++) {
+    console.log('X'.repeat(X));
+
     }
 }

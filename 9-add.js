7df19e63e76e7d9367b19556933566027d3657ab
Enalika <essynalik@gmail.com>
2025-05-24 10:25:41 +0300

    Add file 9-add.js

---
+++ 9-add.js
@@ -0,0 +1,8 @@
+const arg = process.argv.slice(2);
+const firstarg = parseInt(arg[0]);
+const Secondarg = parseInt(arg[1]);
+
+function add(firstarg, Secondarg){
+    return firstarg + Secondarg;
+    }
+     console.log(add(firstarg, Secondarg));
